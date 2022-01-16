5fe3733942408453a895577c7109fc62b19ad3b3
rocket.zhao <rocket.zhao@hp.com>
2022-01-16 15:28:12 +0800

    feat:grahp

--- graph/standard/adjacency_list.go
+++ graph/standard/adjacency_list.go
@@ -2,14 +2,36 @@ package standard
 
 import "container/list"
 
-type AdjacencyList struct {
-	list []*list.List
-	size int
+const m = 5
+
+// Edge : 边定义
+type Edge struct {
+	leftVertex  int
+	rightVertex int
+	vertexInfo  *VertexInfo
 }
 
-func NewAdjacencyList(size int) *AdjacencyList {
-	return &AdjacencyList{
-		list: make([]*list.List, 0, size),
-		size: size,
-	}
+// AdjVNode : 边表结点
+type AdjVNode struct {
+	vertex       int         // 顶点下标
+	vertexINfo   *VertexInfo // 结点信息
+	nextAdjVNode *AdjVNode   // 下一个边表结点
+}
+
+// VNode : 头结点
+type VNode struct {
+	firstEdge *AdjVNode
+	data      interface{}
+}
+
+// VertexInfo : 结点信息
+type VertexInfo struct {
+	weight int
+}
+
+// Graph : 图
+type Graph struct {
+	numOfVertex int
+	numOfEdge   int
+	vNodeLists  []*list.List
 }
--- graph/standard/main/main.go
+++ graph/standard/main/main.go
@@ -7,15 +7,24 @@ import (
 
 func main() {
 
-	linkedList := list.New()
+	var lists []*list.List
 
-	linkedList.PushFront("aaaaaaa")
-	linkedList.PushFront("bbbbbbb")
-	linkedList.PushFront("ccccccc")
-	linkedList.PushFront("ddddddd")
-	linkedList.PushFront("eeeeeee")
+	lists = make([]*list.List, 0)
 
-	fmt.Println(linkedList.Len())
-	fmt.Println(linkedList)
+	linkedList001 := list.New()
+	linkedList001.PushFront("aaaaaa")
+	linkedList001.PushFront("bbbbbb")
+	linkedList001.PushFront("cccccc")
+	linkedList001.PushFront("dddddd")
+
+	linkedList002 := list.New()
+	linkedList002.PushFront("111111")
+	linkedList002.PushFront("222222")
+	linkedList002.PushFront("333333")
+
+	lists = append(lists, linkedList001, linkedList002)
+
+	fmt.Println(lists[0])
+	fmt.Println(lists[1])
 
 }
